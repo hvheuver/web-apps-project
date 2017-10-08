2dcaaf7a3c141be80dfb0e06400caade6f187782
Hendrik <hendrik.vanheuverswyn@skynet.be>
2017-10-08 14:17:48 +0200

    Materialize integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//materialize import
+// https://github.com/InfomediaLtd/angular2-materialize
+// install materialize-css and angular2-materialize
+// import 'materialize-css';
+// ------> https://stackoverflow.com/questions/44584557/integrate-materialize-in-a-angular2-project-with-webpack-and-scss
+// ^best source
+import {MaterializeModule} from 'angular2-materialize';
+
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
