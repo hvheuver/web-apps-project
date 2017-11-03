a6163371d9d8825e2be2a502d738d93f4d9ec978
Hendrik <hendrik.vanheuverswyn@skynet.be>
2017-11-04 00:47:49 +0100

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { MaterializeDirective } from 'angular2-materialize';
+import * as $ from 'jquery' ;
+// https://stackoverflow.com/a/43376844
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'app';
+
+    public ngOnInit(): void {
+/*
+      $(document).ready(function(){
+        $('.carousel').carousel();
+      });
+*/
+    }
 }
